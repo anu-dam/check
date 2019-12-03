ca81d67b5543d2c98e9bd4152f56a041b001aa87
Anuradha Dambaladeniya <anuradhadambaladeniya@Anuradhas-MacBook-Pro.local>
2019-12-04 08:29:58 +1030

    b

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $("#searchBtn").on("click", function(){
 
     var search = $("#select-city").val();
     
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" +search+ "75eb6e5eb20db39b88a78417b81c45f6";
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" +search+ "appid=75eb6e5eb20db39b88a78417b81c45f6";
     
     
     // var APIKey = "75eb6e5eb20db39b88a78417b81c45f6";
