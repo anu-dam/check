b5e3425ddabf8653ae3a52c4a33b878c38d9fa9d
Anuradha Dambaladeniya <anuradhadambaladeniya@Anuradhas-MacBook-Pro.local>
2019-12-04 07:43:52 +1030

    uploading files

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+$("#select-city").on("click", function(){
+
+ 
+
+var search = $("#search").val();
+
+var queryURL = "api.openweathermap.org/data/2.5/weather?zip=94040,us"+search+ "75eb6e5eb20db39b88a78417b81c45f6";
+var APIKey = "75eb6e5eb20db39b88a78417b81c45f6";
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+
+  // Printing the entire object to console
+  console.log(response);
+
+
+});
+
+
+
+
+
+});
