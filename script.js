f321ee2a56a8d16f0ed88d2bc0786ebd8547f85c
Anuradha Dambaladeniya <anuradhadambaladeniya@Anuradhas-MacBook-Pro.local>
2019-12-04 22:41:44 +1030

    CSS amendments

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 
 var city;
-var response;
 
 function callApi(city) {
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=75eb6e5eb20db39b88a78417b81c45f6";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=75eb6e5eb20db39b88a78417b81c45f6";
 
     $.ajax({
         url: queryURL,
@@ -12,14 +11,11 @@ function callApi(city) {
     }).then(function (response) {
 
         $("#city").text("Weather Forcast in " + response.name);
-        $("#temp").text("Temprature " + response.main.temp + "F");
-        $("#humid").text("Humidity " + response.main.humidity);
-        $("#wind").text("Wind Speed " + response.wind.speed);
-        $("#pr").text("Pressure " + response.main.pressure);
-        $("#cond").text("Condition " + response.weather[0].description);
-
-
-        console.log(response);
+        $("#temp").text("Temprature : " + response.main.temp + "C");
+        $("#humid").text("Humidity : " + response.main.humidity);
+        $("#wind").text("Wind Speed : " + response.wind.speed);
+        $("#pr").text("Pressure : " + response.main.pressure);
+        $("#cond").text("Condition : " + response.weather[0].description);
 
         var icon = response.weather[0].icon;
 
@@ -27,11 +23,9 @@ function callApi(city) {
 
         $('#icon').empty();
         $('#icon').append(img);
-
         
     });
 }
-
 
 $("#searchBtn").on("click", function () {
 
@@ -48,3 +42,19 @@ callApi(city);
 
 
 });
+
+function previous(prCity){ 
+
+var previousURL = "http://api.openweathermap.org/data/2.5/forecast?" + City + "&units=metric&appid=75eb6e5eb20db39b88a78417b81c45f6";
+
+$.ajax({
+    url: previousURL,
+    method: "GET"
+}).then(function (response) {
+
+    console.log(response);
+
+});
+}
+
+// https://api.openweathermap.org/data/2.5/forecast?qlondon&mode=xml&appid=75eb6e5eb20db39b88a78417b81c45f6
